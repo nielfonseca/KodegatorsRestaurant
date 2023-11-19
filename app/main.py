1cb1affc147bf197637a75cc66df38f8d6201d9c
Daniel Rodrigues <daniel97rf@gmail.com>
2023-11-18 21:29:35 -0300

    conclusão do login

--- app/main.py
+++ app/main.py
@@ -1,11 +1,5 @@
-import banco_dados
-import sqlite3
-from flask import Flask, render_template, redirect, request
-
-banco = bancod('registros.sqlite')
-banco.criarTabelacliente()
-banco.criarTabelaitem()
-
+import json
+from flask import Flask, render_template, redirect, request, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'KODEGATOR'
@@ -15,32 +9,52 @@ def home():
     
     return render_template('login.html')
 
+@app.route('/adm')
 
-
-@app.route('/login', methods=['POST'])
+@app.route('/cardapio', methods=['POST'])
 def login():
     login = request.form.get('login')
     password = request.form.get('password')
-    
-    if login == 'daniel' and password == 'lucasgay123':
-        return render_template("menu.html")
-    
-    return redirect('/')
+
+    with open('usuarios.json') as usuariosTemp:
+        usuarios = json.load(usuariosTemp)
+        cont = 0
+
+        for usuario in usuarios:
+            cont += 1
+
+            if login == 'admin' and password == 'admin123':
+                return render_template('cadastro.html')
+
+            if usuario['login'] == login and usuario['password'] == password:
+                return render_template("menu.html")
+            if cont >= len(usuarios):
+                flash('USUARIO E SENHA INVALIDOS')
+                return redirect('/')
+
 
 @app.route('/cadastro', methods=['POST'])
 def cadastro():
-    while True:
-        login = request.form.get('login')
-        password = request.form.get('password')
-        new_password = request.form.get('new-password')
-        if password == new_password:
-            banco.inserir(login, password)
-            False
-            return render_template("login.html")
-        else:
-            True           
+    user = []
+    login = request.form.get('login')
+    password = request.form.get('password')
 
+    user= [
+        {
+            "login": login,
+            "password": password
+        }
+    ]
+    
+    with open('usuarios.json') as usuariosTemp:
+        usuarios = json.load(usuariosTemp)
 
+    newlogin = user + usuarios
+
+    with open('usuarios.json', 'w') as gravarTemp:
+        json.dump(newlogin, gravarTemp, indent=4)
+
+    return render_template('login.html')
 
 if __name__ in "__main__":
     app.run(debug=True)
